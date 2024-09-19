8f3f58a0d8f638de69656063b53d3b040cdbe4a7
duchoprog <duchoprog@gmai.com>
2024-09-19 16:23:54 -0300

    modified question in assistant to handle better multi item files

--- assistantTest2.js
+++ assistantTest2.js
@@ -60,7 +60,9 @@ const tableHeaders = [
 const question0 = `This is the question we asked our providers: `;
 const question = `
 Based on the info in your vectorstore.
-I need you to find the relevant information and give it back to me as an array of JSON objects, with one json object(without extra formatting) per size-quantity-incoterm combination present (this means that the amount of jsons you will give me is equal to: [number of sizes offered] x [number of quantities offered] x [incoterm options], if one of them is not informed take its value as 1) with the following attributes:  `;
+I need you to find the relevant information and give it back to me as an array of JSON objects. To generate the objects follow these steps:
+1-identify prices for the items being offered, and count how many prices are there.
+2-for each price, make a json object, looking up the values for the following attributes for each price:  `;
 const question2 = `  If for any  attribute you can't find its value, its value should be NF. The following attributes' values should be only numbers, without currency or units, as they will be used for calculations:"SET UP CHARGE USD", "SAMPLE TIME","PRODUCTION TIME", "QUANTITY", "PRICE USD", "PCS PER BOX", "L", "H", "W", "GW KG". The value for INCOTERM should be one of the following:"EXW", "FOB Shanghai", "FOB Shenzhen", "FOB Ningbo", "FOB Ningbo-Zhoushan", "FOB Hong Kong", "FOB Guangzhou", "FOB Qingdao", "FOB Tianjin", "FOB Dalian", "FOB Xiamen", "FOB Yingkou", "FOB Taizhou", "FOB Yantian", "NF" according to the price cited in the column PRICE USD.The following attributes values should refer to the manufacturer, not to the person asking for the information, the manufacturers are usually Chinese:  "COMPANY NAME", "SALES CONTACT", "WECHAT",  "EMAIL".  Dont wrap this array in a json object.  Sample cost is not equal to setup cost, dont write sample cost in setup cost column. For attribute "PRODUCT REAL DESCRIPTION" copy all information describing the product, dont summarize. Setup cost might be sometimes found in the additional notes. Don't add any other text besides the array of json objects.`;
 let answer = [];
 
