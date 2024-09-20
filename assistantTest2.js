7f45e962c8d80e31a9ddfaa0060c117962e9f4c3
duchoprog <duchoprog@gmai.com>
2024-09-20 08:38:43 -0300

    Improved prompt, changed togpt-4o

--- assistantTest2.js
+++ assistantTest2.js
@@ -1,3 +1,6 @@
+/////////////////////////////////
+///  gpt-4o  ///////////////////
+////////////////////////////////
 const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
@@ -60,10 +63,18 @@ const tableHeaders = [
 const question0 = `This is the question we asked our providers: `;
 const question = `
 Based on the info in your vectorstore.
-I need you to find the relevant information and give it back to me as an array of JSON objects. To generate the objects follow these steps:
-1-identify prices for the items being offered, and count how many prices are there.
+I need you to find the relevant information and give it back to me as an array of JSON objects. 
+To generate the objects follow these steps:
+1-identify prices for the items being offered, and count how many prices are there. Use all these prices as guide for step 2
 2-for each price, make a json object, looking up the values for the following attributes for each price:  `;
-const question2 = `  If for any  attribute you can't find its value, its value should be NF. The following attributes' values should be only numbers, without currency or units, as they will be used for calculations:"SET UP CHARGE USD", "SAMPLE TIME","PRODUCTION TIME", "QUANTITY", "PRICE USD", "PCS PER BOX", "L", "H", "W", "GW KG". The value for INCOTERM should be one of the following:"EXW", "FOB Shanghai", "FOB Shenzhen", "FOB Ningbo", "FOB Ningbo-Zhoushan", "FOB Hong Kong", "FOB Guangzhou", "FOB Qingdao", "FOB Tianjin", "FOB Dalian", "FOB Xiamen", "FOB Yingkou", "FOB Taizhou", "FOB Yantian", "NF" according to the price cited in the column PRICE USD.The following attributes values should refer to the manufacturer, not to the person asking for the information, the manufacturers are usually Chinese:  "COMPANY NAME", "SALES CONTACT", "WECHAT",  "EMAIL".  Dont wrap this array in a json object.  Sample cost is not equal to setup cost, dont write sample cost in setup cost column. For attribute "PRODUCT REAL DESCRIPTION" copy all information describing the product, dont summarize. Setup cost might be sometimes found in the additional notes. Don't add any other text besides the array of json objects.`;
+const question2 = `  If for any  attribute you can't find its value, its value should be NF. The following attributes' values should be only numbers, without currency or units, as they will be used for calculations:"SET UP CHARGE USD", "SAMPLE TIME","PRODUCTION TIME", "QUANTITY", "PRICE USD", "PCS PER BOX", "L", "H", "W", "GW KG". The value for INCOTERM should be one of the following:"EXW", "FOB Shanghai", "FOB Shenzhen", "FOB Ningbo", "FOB Ningbo-Zhoushan", "FOB Hong Kong", "FOB Guangzhou", "FOB Qingdao", "FOB Tianjin", "FOB Dalian", "FOB Xiamen", "FOB Yingkou", "FOB Taizhou", "FOB Yantian", "NF" according to the price cited in the column PRICE USD.The following attributes values should refer to the manufacturer, not to the person asking for the information, the manufacturers are usually Chinese:  "COMPANY NAME", "SALES CONTACT", "WECHAT",  "EMAIL".  Dont wrap this array in a json object.  Sample cost is not equal to setup cost, dont write sample cost in setup cost column. For attribute "PRODUCT REAL DESCRIPTION" copy the product description and any other relevant info related to the product that hasn't been included in another attribute. Setup cost might be sometimes found in the additional notes. Don't add any other text besides the array of json objects.`;
+
+/* const question0 = `This is the question we asked our providers: `;
+const question = `
+Based on the info in your vectorstore.
+1-identify prices for the items being offered, and count how many prices are there. Make a list of prices and add to each price the rest of the info present. Remember, the focus is on the prices, the item name is merely one of the attributes of the price. For each price add the information mentioned in this list:  `;
+const question2 = `  When the information apparently repeats from previous price check it twice means that you need to recheck because NEVER will be different prices for same item unless something changes, maybe the quantity or maybe the materials or any other quality,  never use this phrase or one similar"All other details same as above", because its wrong. Return the list of prices. Remember, I want a list of prices, with the attributes associated to each price. NOT a list of items`; */
+
 let answer = [];
 
 let fileToProcess;
